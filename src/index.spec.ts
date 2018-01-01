92a9d31b8ed730a57e331d84a8bf64474ae6ff20
juniperoserra <simon.greenwold@gmail.com>
2018-01-01 17:22:24 -0500

    Added tslint and standard config

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,16 @@
-import 'mocha';
-import * as chai from 'chai';
-const dirtyChai : any = require('dirty-chai'); // Turns chai expectations into functions instead of property access
-chai.use(dirtyChai);
-const {expect} = chai;
+import 'mocha'
+import * as chai from 'chai'
+const dirtyChai: any = require('dirty-chai') // Turns chai expectations into functions instead of property access
+chai.use(dirtyChai)
+const { expect } = chai
 
-import { hello } from './index';
+import { hello } from './index'
 
 describe('Hello function', () => {
 
   it('should return hello world', () => {
-    const result = hello();
-    expect(result).to.equal('Hello world!');
-  });
+    const result = hello()
+    expect(result).to.equal('Hello world!')
+  })
 
-});
+})
